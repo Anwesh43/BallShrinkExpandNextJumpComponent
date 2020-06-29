a4c6058da450555defa9cc4c65aaa903caea1ecd
Anwesh43 <anweshthecool0@gmail.com>
2020-06-30 02:31:22 +0530

    adding useStyle hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,5 @@
 import {useState, useEffect} from 'react'
+import {dividesScale, sinify} from './utils'
 
 export const useAnimatedScale = (scGap, delay, n) => {
     const [scale, setScale] = useState(0)
@@ -50,4 +51,25 @@ export const useDimension = () => {
         w, 
         h,
     } 
+}
+
+export const useStyle = (i, w, h, scale, n) => {
+    const size = w / 5 
+    const background = '#3F51B5'
+    const sc1 = divideScale(scale, 0, 2)
+    const sc2 = divideScale(scale, 1, 2)
+    const sf1 = sinify(sc1)
+    const position = 'absolute'
+    const width = `${size * sf1}px`
+    const height = `${size * sf1}px`
+    const gap = w / (n + 1)
+    const top = `${h / 2 - size / 2}px`
+    const left = `${i * gap + gap * sc2}px`
+    const borderRadius = '50%'
+    return {
+        getSinifiedCircleStyle() {
+            return {position, backgroud, width, height, top, left, borderRadius}
+        }
+    }
+
 }
